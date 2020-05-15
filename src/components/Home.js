ee75ec82ac26cf4d54a7e5e5087011db88a0f90c
edenfuller <equinoxfires@gmail.com>
2020-05-15 12:06:13 -0400

    Add Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+function Home() {
+    return(
+        <div id="home">   
+            <h1>{'Eden\'s Data Structures & Algorithms Workbook'}</h1>
+        </div>
+    )
+}
+
+export default Home;
