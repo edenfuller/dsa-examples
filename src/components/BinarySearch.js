53aecc19e60e177ab7d890bfb8e869634668ac55
edenfuller <equinoxfires@gmail.com>
2020-05-15 12:44:46 -0400

    Add inputs for array length and search value

--- src/components/BinarySearch.js
+++ src/components/BinarySearch.js
@@ -3,18 +3,20 @@ import React, { useState } from 'react';
 function BinarySearch() {
 
 
-  const [arrayLength, setArrayLength] = useState(6);
-
+  const [arrayLength, setArrayLength] = useState(8);
   const [x, setX] = useState(5);
-
   const [valueIndex, setValueIndex] = useState(0)
+  const [start, setStart] = useState(0);
+  const [end, setEnd] = useState(7);
 
   const returnArray = (targetLength) => {
       let arr = [];
       while (arr.length < targetLength){
           const newValue = Math.floor(Math.random() * targetLength);
-          if (arr.indexOf(newValue) !== -1) arr.push(newValue);
+          console.log(`checking for presence of ${newValue} in array...`);
+          if (arr.indexOf(newValue) === -1) arr.push(newValue);
       }
+      console.log(arr);
       return arr;
 
   }
@@ -31,7 +33,7 @@ function BinarySearch() {
             let mid=Math.floor((start + end)/2); 
        
             // If element is present at mid, return True 
-            if (arr[mid]===x) return true; 
+            if (arr[mid]===x) return x; 
       
             // Else look in left or right half accordingly 
             else if (arr[mid] < x)  
@@ -40,12 +42,19 @@ function BinarySearch() {
                  end = mid - 1; 
         } 
        
-        return false; 
+        return -1; 
     } 
        
     return(
         <div id="binary-search">   
             <h1>{'Binary Search'}</h1>
+            <p>Length of array:</p>
+            <input value={arrayLength} onChange={(e) => {setArrayLength(e.target.value)}}></input>
+            <p>Value to search for:</p>
+            <input value={x} onChange={(e) => {setX(e.target.value)}}></input>
+            <p></p>
+            <button onClick={() => {setValueIndex(binarySearch(returnArray(arrayLength), x))}}>Search</button>
+            <p>{`Index of target value is ${valueIndex}`}</p>
         </div>
     )
 }
