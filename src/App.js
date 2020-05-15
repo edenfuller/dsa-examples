514ddcf9b5c9ef1c9ced58ead1d3762853fe9c73
edenfuller <equinoxfires@gmail.com>
2020-05-15 12:09:54 -0400

    Add router to App.js, add BinarySearch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Home from './components/Home.js';
+import BinarySearch from './components/BinarySearch.js';
+
 import './App.css';
 
 function App() {
+
+  const [route, setRoute] = useState('home');
+
   return (
     <div className="App">
-     <h1>{'Eden\'s Data Structures & Algorithms Workbook'}</h1>
+      <div id="inner" >
+
+     <Router>
+
+          <nav>
+            <ul>
+              <li>
+                <Link className={route === "home" ? "selected" : ''} onClick={() => { setRoute('home'); }} to="/">Home</Link>
+              </li>
+              <li>
+                <Link className={route === "binary-search" ? "selected" : ''} onClick={() => { setRoute('binary-search'); }} to="/binary-search">Binary Search</Link>
+              </li>
+            </ul>
+          </nav>
+
+
+          <Switch>
+
+            <Route path="/binary-search">
+              <BinarySearch setRoute={setRoute} />
+            </Route>
+            <Route path="*">
+              <Home setRoute={setRoute} />
+            </Route>
+          </Switch>
+        </Router>
+        </div>
     </div>
   );
 }
---
+++ src/components/BinarySearch.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+function BinarySearch() {
+    return(
+        <div id="binary-search">   
+            <h1>{'Binary Search'}</h1>
+        </div>
+    )
+}
+
+export default BinarySearch;
